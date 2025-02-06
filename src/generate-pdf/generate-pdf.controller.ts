ea995acf5060ec8c423d6858f1994fdf4ec6b72b
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-02-06 15:00:37 +0100

    install carbone and create service

--- src/generate-pdf/generate-pdf.controller.ts
+++ src/generate-pdf/generate-pdf.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Res } from '@nestjs/common';
 import { GeneratePdfService } from './generate-pdf.service';
-import { CreateGeneratePdfDto } from './dto/create-generate-pdf.dto';
-import { UpdateGeneratePdfDto } from './dto/update-generate-pdf.dto';
+import { Response } from 'express';
 
 @Controller('generate-pdf')
 export class GeneratePdfController {
   constructor(private readonly generatePdfService: GeneratePdfService) {}
 
   @Post()
-  create(@Body() createGeneratePdfDto: CreateGeneratePdfDto) {
-    return this.generatePdfService.create(createGeneratePdfDto);
+  create(@Res() res: Response) {
+    return this.generatePdfService.create();
   }
 
   @Get()
   findAll() {
     return this.generatePdfService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.generatePdfService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGeneratePdfDto: UpdateGeneratePdfDto) {
-    return this.generatePdfService.update(+id, updateGeneratePdfDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.generatePdfService.remove(+id);
   }
 }
--- src/generate-pdf/generate-pdf.service.ts
+++ src/generate-pdf/generate-pdf.service.ts
@@ -1,26 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { CreateGeneratePdfDto } from './dto/create-generate-pdf.dto';
-import { UpdateGeneratePdfDto } from './dto/update-generate-pdf.dto';
 
 @Injectable()
 export class GeneratePdfService {
-  create(createGeneratePdfDto: CreateGeneratePdfDto) {
-    return 'This action adds a new generatePdf';
+  create() {
+    const payload = {
+      firsName: ' Juan',
+      lastName: 'Carlos',
+    };
+
+    return payload;
   }
 
   findAll() {
-    return `This action returns all generatePdf`;
+    const payload = {
+      firsName: ' Juan',
+      lastName: 'Carlos',
+    };
+
+    return payload;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} generatePdf`;
-  }
-
-  update(id: number, updateGeneratePdfDto: UpdateGeneratePdfDto) {
-    return `This action updates a #${id} generatePdf`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} generatePdf`;
   }
 }
