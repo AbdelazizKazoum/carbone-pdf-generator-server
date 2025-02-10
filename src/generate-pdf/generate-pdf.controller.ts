7be5e9759a5137187fb90036a8f0c37ed6ff9cad
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-02-10 23:40:55 +0100

    add default data

--- src/generate-pdf/generate-pdf.controller.ts
+++ src/generate-pdf/generate-pdf.controller.ts
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Res } from '@nestjs/common';
 import { GeneratePdfService } from './generate-pdf.service';
-import { response, Response } from 'express';
+import { Response } from 'express';
 
 @Controller('generate-pdf')
 export class GeneratePdfController {
@@ -14,18 +15,27 @@ export class GeneratePdfController {
   @Get()
   async render(@Res() res: Response) {
     const payload = {
-      firsName: ' Juan',
-      lastName: 'Carlos',
-      region: 'Meknes',
-      datePV: '2012/02/12',
-      companie: 'Meknes',
-      center: 'Rabat',
+      region: 'طنجة',
+      compagnie: 'طنجة',
+      centre: 'القضائي بطنجة',
+      numero_pv: 12345,
+      date_pv: '2025-02-07',
+      nom_personne_impliquee: 'مراد الصروخ',
+      nom_officier_premier: 'عبدالرحيم أوموي',
+      grade_officier_premier: 'رقيب',
+      statut_officier_premier: '(ض.ش.ق)',
+      nom_officier_deuxieme: 'خالد بوماي',
+      grade_officier_deuxieme: 'رقيب أول',
+      statut_officier_deuxieme: '(ع.ش.ق)',
+      accusation: 'محاولة الهجرة غير المشروع',
+      article_code: '20 إلى 26 و 78',
+      article_loi: 'المسطرة الجنائية',
     };
 
     const pdfBuffer = await this.generatePdfService.generate(
       payload,
       'template.odt',
-      'docx',
+      'pdf',
     );
 
     res.setHeader('Content-Type', 'application/pdf');
