e1e518e5f20f114ce3b78b4a32b7850bde3d6e8d
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-02-12 14:47:03 +0100

    modfy request method

--- src/generate-pdf/generate-pdf.controller.ts
+++ src/generate-pdf/generate-pdf.controller.ts
@@ -1,5 +1,10 @@
-/* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { GeneratePdfService } from './generate-pdf.service';
 import { Response } from 'express';
 
@@ -7,41 +12,43 @@ import { Response } from 'express';
 export class GeneratePdfController {
   constructor(private readonly generatePdfService: GeneratePdfService) {}
 
+  // @Post()
+  // create(@Res() res: Response) {
+  //   return this.generatePdfService.create();
+  // }
+
   @Post()
-  create(@Res() res: Response) {
-    return this.generatePdfService.create();
-  }
+  async render(@Body() body, @Res() res: Response) {
+    // const payload = {
+    //   region: 'بطنجة',
+    //   compagnie: 'طنجة',
+    //   centre: 'القضائي بطنجة',
+    //   numero_pv: 12345,
+    //   date_pv: '2025-02-07',
+    //   nom_personne_impliquee: 'مراد الصروخ',
+    //   nom_officier_premier: 'عبدالرحيم أوموي',
+    //   grade_officier_premier: 'رقيب',
+    //   statut_officier_premier: '(ض.ش.ق)',
+    //   nom_officier_deuxieme: 'خالد بوماي',
+    //   grade_officier_deuxieme: 'رقيب أول',
+    //   statut_officier_deuxieme: '(ع.ش.ق)',
+    //   accusation: 'محاولة الهجرة غير المشروع',
+    //   article_code: '20 إلى 26 و 78',
+    //   article_loi: 'المسطرة الجنائية',
+    // };
 
-  @Get()
-  async render(@Res() res: Response) {
-    const payload = {
-      region: 'طنجة',
-      compagnie: 'طنجة',
-      centre: 'القضائي بطنجة',
-      numero_pv: 12345,
-      date_pv: '2025-02-07',
-      nom_personne_impliquee: 'مراد الصروخ',
-      nom_officier_premier: 'عبدالرحيم أوموي',
-      grade_officier_premier: 'رقيب',
-      statut_officier_premier: '(ض.ش.ق)',
-      nom_officier_deuxieme: 'خالد بوماي',
-      grade_officier_deuxieme: 'رقيب أول',
-      statut_officier_deuxieme: '(ع.ش.ق)',
-      accusation: 'محاولة الهجرة غير المشروع',
-      article_code: '20 إلى 26 و 78',
-      article_loi: 'المسطرة الجنائية',
-    };
+    try {
+      const pdfBuffer = await this.generatePdfService.generate(
+        body,
+        'template.odt',
+        'pdf',
+      );
 
-    const pdfBuffer = await this.generatePdfService.generate(
-      payload,
-      'template.odt',
-      'pdf',
-    );
-
-    res.setHeader('Content-Type', 'application/pdf');
-    // res.setHeader('Content-Disposition', 'attachment; filename=output.pdf');
-    res.send(pdfBuffer);
-
-    return payload;
+      res.setHeader('Content-Type', 'application/pdf');
+      // res.setHeader('Content-Disposition', 'attachment; filename=output.pdf');
+      res.send(pdfBuffer);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
